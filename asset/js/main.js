9e4eb4842542a79ad9ecc0bee55d144f7e283325
jerrsu <jerrysuparman@gmail.com>
2026-02-05 01:54:30 +0700

    asd

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,9 +1,9 @@
-AOS.init({
-  duration: 800,
-  once: true
-});
+// AOS.init({
+//   duration: 800,
+//   once: true
+// });
 
-window.addEventListener('scroll', () => {
-  document.querySelector('.navbar')
-    .classList.toggle('scrolled', window.scrollY > 50);
-});
+// window.addEventListener('scroll', () => {
+//   document.querySelector('.navbar')
+//     .classList.toggle('scrolled', window.scrollY > 50);
+// });
