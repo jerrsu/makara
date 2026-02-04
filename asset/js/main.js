6203059d0ec7c1462158405aaeb62f02656f0748
jerrsu <jerrysuparman@gmail.com>
2026-02-05 01:24:44 +0700

    baru

---
+++ asset/js/main.js
@@ -0,0 +1,9 @@
+AOS.init({
+  duration: 800,
+  once: true
+});
+
+window.addEventListener('scroll', () => {
+  document.querySelector('.navbar')
+    .classList.toggle('scrolled', window.scrollY > 50);
+});
